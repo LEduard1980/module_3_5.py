515e4919639aa3aeb908f3a3a459b632ad56fac7
LEduard1980 <lebedev1_80@mail.ru>
2024-09-07 20:23:16 +0300

    numbers

---
+++ main.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+Primes = []
+Not_Primes = []
+for is_prime in numbers:
+    if is_prime > 1:
+        for i in range (2, is_prime):
+            if (is_prime % i) == 0:
+                Not_Primes.append(is_prime)
+                break
+        else:
+            Primes.append(is_prime)
+    else:
+        Not_Primes.append(is_prime)
+print('Primes:', Primes)
+print('Not_Primes:', Not_Primes)
+
+
+
+
+
+
+
